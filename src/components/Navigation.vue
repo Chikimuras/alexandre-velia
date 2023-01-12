4c72467989372b3809e361637effd67bb6de3821
Alexandre <alexandre.m.velia@gmail.com>
2023-01-12 17:21:48 +0100

    Create the first link of the navbar and create style for it

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,14 @@
 <template>
   <header>
-    <div>
-      <div class="logo">
-        <a href="/"><img width="150" src="@/assets/logo.svg" alt="logo" /></a>
+    <div id="navbar">
+      <div id="logo">
+        <a href="/" class="logo">Alexandre Velia</a>
       </div>
       <nav>
-        <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/"><span>Home</span></RouterLink>
+        <RouterLink to="/portfolio"><span>Portfolio</span></RouterLink>
+        <RouterLink to="/contact"><span>Contact</span></RouterLink>
+        <RouterLink to="/blog"><span>Blog</span></RouterLink>
       </nav>
     </div>
   </header>
@@ -19,5 +22,48 @@ export default {
 </script>
 
 <style scoped>
+  header{
+    padding-block: 1.8rem;
+  }
+  #logo{
+    display: inline-block;
+    margin-right: 1.8rem;
+  }
+  #navbar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+  }
+  nav a{
+    background: var(--gradient);
+    padding: 0.5rem 1rem;
+    border-radius: .2rem;
+    text-decoration: none;
+    text-align: center;
+    position: relative;
+    margin-inline: .5rem;
+  }
+  nav a:before{
+    content: "";
+    position: absolute;
+    inset: 0;
+    border-radius: .15rem;
+    background: #fff;
+    transition: inset .1s ease-in-out;
+  }
+  nav a span{
+    text-transform: uppercase;
+    color: #000000;
+    font-size: .875rem;
+    font-weight: 600;
+    z-index: 2;
+    position: relative;
+  }
 
+  nav a:hover::before,
+  nav a.router-link-active::before,
+  nav a.router-link-exact-active::before{
+    inset: 2px;
+  }
 </style>
